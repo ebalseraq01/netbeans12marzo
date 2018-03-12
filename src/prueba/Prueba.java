41f67223c29d2e6949a68780dfa47c5097afff21
ebalseraq01 <ennybalsera@gmail.com>
2018-03-12 11:51:31 +0100

    subida3-hola

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola22");
     }
     
 }
